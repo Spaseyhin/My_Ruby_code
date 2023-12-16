fcadf8a3f43d078361857aa209136869cb188d74
Spaseyhin <evgen.kalin99@gmail.com>
2023-12-16 18:22:07 +0500

    refactoring

--- ruby_code/Lesson_58/main.rb
+++ ruby_code/Lesson_58/main.rb
@@ -3,13 +3,10 @@ films = File.open(films_path,'r').readlines
 
 
 system 'clear'
-
-odd_films = films.values_at(* films.each_index.select(&:odd?))
 even_films = films.values_at(* films.each_index.select(&:even?))
 random_film = even_films.sample
-current_index = films.index(random_film)
-next_film = films [current_index + 1]
+next_films = films[films.index(random_film).to_i.next]
+
 
 puts "Сегодня вы будете смотреть: #{random_film}"
 puts "Описание: \n#{next_film}"
-
--- ruby_code/Visilisa_les57/kek.rb
+++
@@ -1,7 +0,0 @@
-x = 111
-
-case x 
-	when x == 111
-		puts 'хуй'
-	end
-end
